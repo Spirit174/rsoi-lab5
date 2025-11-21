72eb62e943cf9e85c0f2cb9c4a036225840ac651
spirit2174 <zemtsov04@yandex.ru>
2025-11-22 01:49:48 +0300

    try

--- src/Booking.System/Booking.System.LoyaltyService/Startup.cs
+++ src/Booking.System/Booking.System.LoyaltyService/Startup.cs
@@ -93,6 +93,7 @@ public class Startup
             c.SwaggerEndpoint("/api/v1/swagger/v1/swagger.json", "Person.Server.Http v1");
             c.RoutePrefix = "api/v1/swagger";
         });
+        app.UseRouting();
         app.UseAuthentication();
         app.UseAuthorization();
 
