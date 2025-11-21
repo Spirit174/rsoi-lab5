1c38f9d0f979e8d1ea06958b7b40c7b565424015
spirit2174 <zemtsov04@yandex.ru>
2025-11-22 01:03:56 +0300

    try

--- src/Booking.System/Booking.System.Gateway/Startup.cs
+++ src/Booking.System/Booking.System.Gateway/Startup.cs
@@ -80,7 +80,6 @@ public class Startup
         services.AddSingleton<ILoyaltyClient, LoyaltyClient>();
         services.AddSingleton<IPaymentClient, PaymentClient>();
         services.AddSingleton<IReservationClient, ReservationClient>();
-        services.AddHttpClient();
         services.AddHttpContextAccessor();
         services.AddScoped<IUserContext, UserContext>();
         
